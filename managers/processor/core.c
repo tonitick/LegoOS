97468f82fef7036543340871703d1ce095167bc0
Zheng Zhong <tonitick@hotmail.com>
2019-08-22 13:57:55 -0600

    use python3

--- managers/processor/core.c
+++ managers/processor/core.c
@@ -64,7 +64,7 @@ static int procmgmt(void *unused)
 #endif
 
 #if 1
-	init_filename = "/usr/bin/python";
+	init_filename = "/usr/bin/python3";
 	argv_init[0] = init_filename;
 	// argv_init[1] = "/root/ys/models-1.4.0/official/resnet/cifar10_main.py";
 	argv_init[1] = "/root/tf.py";
