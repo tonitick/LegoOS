582de5c988ef1291542d0ca725e0783588680d14
Zheng Zhong <tonitick@hotmail.com>
2019-08-23 02:21:18 -0600

    modify script name

--- managers/processor/core.c
+++ managers/processor/core.c
@@ -68,7 +68,7 @@ static int procmgmt(void *unused)
 	init_filename = "/root/Python-3.6.7/python";
 	argv_init[0] = init_filename;
 	// argv_init[1] = "/root/ys/models-1.4.0/official/resnet/cifar10_main.py";
-	argv_init[1] = "/root/tf.py";
+	argv_init[1] = "/root/tf_test_import.py";
 #endif
 
 	/*
