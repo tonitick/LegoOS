a234b8380f6b03e6d60b3249fff1d2929b1fd71f
Zheng Zhong <tonitick@hotmail.com>
2019-09-11 02:45:56 -0600

    install python 3.6.8 to system

--- managers/processor/core.c
+++ managers/processor/core.c
@@ -65,7 +65,8 @@ static int procmgmt(void *unused)
 
 #if 1
 	// init_filename = "/usr/bin/python";
-	init_filename = "/root/Python-3.6.8/python";
+	// init_filename = "/root/Python-3.6.8/python";
+	init_filename = "/usr/local/bin/python3";
 	argv_init[0] = init_filename;
 	// argv_init[1] = "/root/ys/models-1.4.0/official/resnet/cifar10_main.py";
 	argv_init[1] = "/root/tf_test_import.py";
