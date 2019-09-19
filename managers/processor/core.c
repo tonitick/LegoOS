0e660a6dc2c5aed735636f942136f167eed3489e
zzhong <zzhong@node-0.tflego.pennnetworks-pg0.apt.emulab.net>
2019-09-19 03:02:45 -0600

    use 3.5.7

--- managers/processor/core.c
+++ managers/processor/core.c
@@ -65,8 +65,7 @@ static int procmgmt(void *unused)
 
 #if 1
 	// init_filename = "/usr/bin/python";
-	// init_filename = "/root/Python-3.6.8/python";
-	init_filename = "/usr/local/bin/python3";
+	init_filename = "/root/Python-3.5.7/python";
 	argv_init[0] = init_filename;
 	// argv_init[1] = "/root/ys/models-1.4.0/official/resnet/cifar10_main.py";
 	argv_init[1] = "/root/tf_test_import.py";
